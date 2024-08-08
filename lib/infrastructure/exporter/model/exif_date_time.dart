fcab47fe528af5fe41320ae4d5e7aa4a3faea876
Alexandre Picavet <apicavet@hotmail.fr>
2024-08-08 14:24:17 +0200

    fix(exiftool-export): Fix Longitude reference and Timezone
    Fixes #1 #2

--- lib/infrastructure/exporter/model/exif_date_time.dart
+++ lib/infrastructure/exporter/model/exif_date_time.dart
@@ -1,8 +1,12 @@
 import 'package:location_logger/common/model/int_extension.dart';
 
 extension ExifDateTime on DateTime {
-  String toExif() => [
-        [year.toString(), month.digits(2), day.digits(2)],
-        [hour.digits(2), minute.digits(2), second.digits(2)]
-      ].map((parts) => parts.join(":")).join(" ");
+  String toExif() {
+    final utc = toUtc();
+
+    return [
+      [utc.year.toString(), utc.month.digits(2), utc.day.digits(2)],
+      [utc.hour.digits(2), utc.minute.digits(2), utc.second.digits(2)]
+    ].map((parts) => parts.join(":")).join(" ");
+  }
 }
